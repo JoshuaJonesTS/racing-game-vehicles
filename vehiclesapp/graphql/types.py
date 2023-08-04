2e4ccfa6361f7583c05e5c78790e6c1d6bb39c10
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-08-03 19:41:24 -0700

    Create types.py
    types.py depends on resolvers.py, so the former is most logically created after the latter

---
+++ vehiclesapp/graphql/types.py
@@ -0,0 +1,14 @@
+from graphene_django import DjangoObjectType
+from vehicles.models import Vehicle
+import graphene
+from .resolvers import vehicle
+
+@key("id")
+class VehicleType(DjangoObjectType):
+
+    class Meta:
+        model = Vehicle
+        fields = ("__all__")
+
+    def _resolve_reference(self, info):
+        return Vehicle.objects.get(id=self.id)
