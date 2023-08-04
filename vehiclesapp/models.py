f54115831cfd31226aa7430852485a0f8c39d77b
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-08-03 19:24:53 -0700

    Update models.py
    Create model (class) for vehicles.

--- vehiclesapp/models.py
+++ vehiclesapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+import uuid
+from django.db import models
+
+class Vehicle(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=15)
+    speed = models.IntegerField()
+    acceraltion = models.IntegerField()
+    durability = models.IntegerField()
+    handling = models.IntegerField()
+    traction = models.IntegerField()
+
+    def __str__(self):
+        return self.name
