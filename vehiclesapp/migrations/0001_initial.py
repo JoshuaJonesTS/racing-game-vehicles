b5076aabdd709963f2b21293398d31595e78cd67
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-08-04 12:56:04 -0700

    Fix field name

--- vehiclesapp/migrations/0001_initial.py
+++ vehiclesapp/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                 ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False, unique=True)),
                 ('name', models.CharField(max_length=15)),
                 ('speed', models.IntegerField()),
-                ('acceraltion', models.IntegerField()),
+                ('acceleration', models.IntegerField()),
                 ('durability', models.IntegerField()),
                 ('handling', models.IntegerField()),
                 ('traction', models.IntegerField()),
