7a34b23bc5fc15adfadeca3358235b27888d31af
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-08-03 19:42:57 -0700

    Create schema.py
    schemas.py depends on both types.py and resolvers.py, so that ought to be created third

---
+++ vehiclesapp/graphql/schema.py
@@ -0,0 +1,14 @@
+import graphene
+from vehicles.models import Vehicle
+from .types import *
+from .resolvers import *
+
+class VehicleQueries(graphene.ObjectType):
+    all_vehicles = graphene.List(VehicleType)
+    vehicle = graphene.Field(VehicleType, id=graphene.ID())
+
+    def resolve_all_vehicles(cls, info):
+        return all_vehicles()
+    
+    def resolve_vehicle(cls, info, id):
+        return vehicle(id)
