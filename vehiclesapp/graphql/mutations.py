8b0e5fbced6c8a801d690054ae0a454bf67f1ae8
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-08-03 19:45:14 -0700

    Create mutations.py
    mutations.py depends on type.py, so it should be either the 3rd or 4th created out of the four graphql files (excluding __init__.py).

---
+++ vehiclesapp/graphql/mutations.py
@@ -0,0 +1,37 @@
+import graphene
+from .types import VehicleType
+from vehicles.models import Vehicle
+
+class CreateVehicle(graphene.Mutation):
+    class Arguments():
+        name = graphene.String(required=True)
+    
+    vehicle = graphene.Field(VehicleType)
+
+    @classmethod
+    def mutate(cls, root, info, name):    
+        new_vehicle = Vehicle.objects.create(name=name)
+        return CreateVehicle(vehicle=new_vehicle)
+    
+class EditVehicle(graphene.Mutation):
+    class Arguments():
+        id = graphene.ID(required=True)
+        name = graphene.String(require=True)
+
+    vehicle = graphene.Field(VehicleType)
+
+    @classmethod
+    def mutate(cls, root, info, id, name):
+        try:
+            vehicle = Vehicle.objects.get(pk=id)
+        except Vehicle.DoesNotExist:
+            raise Exception('Vehicle does not exist.')
+        
+        vehicle.name = name
+        vehicle.save()
+
+        return EditVehicle(vehicle=vehicle)
+    
+class VehicleMutations(graphene.ObjectTypy):
+    create_vehicle = CreateVehicle.Field()
+    edit_vehicle = EditVehicle.Field()
