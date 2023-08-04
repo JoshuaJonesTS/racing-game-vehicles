e9826b9cf38c556ed2a9a4f2994e29d147a7b46c
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-08-03 19:22:27 -0700

    Update urls.py
    Add conditional involving STAGE.

--- vehiclesproject/urls.py
+++ vehiclesproject/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.STAGE == 'PRODUCTION':
+    urlpatterns.append(path('', csrf_exempt(GraphQLView.as_view(graphiql=False))))
+
+else:
+    urlpatterns.append(path('', csrf_exempt(GraphQLView.as_view(graphiql=True))))
