a00a51c20208d70552888d0be08e514dd6a0aa32
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-08-04 12:39:29 -0700

    Update mutations.py
    Add other fields

--- vehiclesapp/graphql/mutations.py
+++ vehiclesapp/graphql/mutations.py
@@ -5,13 +5,25 @@ from vehiclesapp.models import Vehicle
 class CreateVehicle(graphene.Mutation):
     class Arguments():
         name = graphene.String(required=True)
+        speed = graphene.Int(required=True)
+        acceleration = graphene.Int(required=True)
+        durability = graphene.Int(required=True)
+        handling = graphene.Int(required=True)
+        traction = graphene.Int(required=True)
     
     vehicle = graphene.Field(VehicleType)
 
     @classmethod
-    def mutate(cls, root, info, name):    
-        new_vehicle = Vehicle.objects.create(name=name)
-        return CreateVehicle(vehicle=new_vehicle)
+    def mutate(cls, root, info, vehicle_data=None):    
+        new_vehicle = Vehicle(
+            name = vehicle_data.name,
+            speed = vehicle_data.speed,
+            acceleration = vehicle_data.acceleration,
+            durability = vehicle_data.acceleration,
+            handling = vehicle_data.handling,
+            traction = vehicle_data.traction
+        )
+        return CreateVehicle(new_vehicle=new_vehicle)
     
 class EditVehicle(graphene.Mutation):
     class Arguments():
