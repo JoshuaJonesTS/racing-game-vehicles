a5448835d82bcf3ebf696bfae4b3e50958004074
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-08-04 18:34:28 -0700

    Update mutations.py
    Vehicles can now be edited!

--- vehiclesapp/graphql/mutations.py
+++ vehiclesapp/graphql/mutations.py
@@ -26,23 +26,42 @@ class CreateVehicle(graphene.Mutation):
         return CreateVehicle(vehicle=new_vehicle)
     
 class EditVehicle(graphene.Mutation):
-    class Arguments():
-        id = graphene.ID(required=True)
-        name = graphene.String(required=True)
+    class Arguments:
+        id = graphene.UUID(required=True)
+        name = graphene.String()
+        # speed = graphene.Int(),
+        acceleration = graphene.Int()
+        durability = graphene.Int()
+        handling = graphene.Int()
+        traction = graphene.Int()
 
     vehicle = graphene.Field(VehicleType)
 
     @classmethod
-    def mutate(cls, root, info, id, name):
-        try:
-            vehicle = Vehicle.objects.get(pk=id)
-        except Vehicle.DoesNotExist:
-            raise Exception('Vehicle does not exist.')
-        
-        vehicle.name = name
-        vehicle.save()
+    def mutate(cls, root, info, **kwargs):        
+        new_vehicle = Vehicle.objects.get(pk=kwargs.get('id'))
 
-        return EditVehicle(vehicle=vehicle)
+        if 'name' in kwargs:
+            new_vehicle.name=kwargs.get('name') 
+
+        if 'speed' in kwargs:
+            new_vehicle.speed=kwargs.get('speed') 
+
+        if 'acceleration' in kwargs:
+            new_vehicle.acceleration=kwargs.get('acceleration') 
+
+        if 'durability' in kwargs:
+            new_vehicle.durability=kwargs.get('durability') 
+
+        if 'handling' in kwargs:
+            new_vehicle.handling=kwargs.get('handling') 
+
+        if 'traction' in kwargs:
+            new_vehicle.traction=kwargs.get('traction') 
+
+        new_vehicle.save()
+
+        return EditVehicle(vehicle=new_vehicle)
     
 class VehicleMutations(graphene.ObjectType):
     create_vehicle = CreateVehicle.Field()
